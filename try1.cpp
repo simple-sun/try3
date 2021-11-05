3b10dd8ad523eb767d4b0c790fe137e72b0cb103
simple-sun <simplessq@163.com>
2021-11-04 18:54:31 -0700

    bad mood

---
+++ try1.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************
+> File Name:     try1.cpp
+> Author:        程序员Carl
+> 微信公众号:    代码随想录
+> Created Time:  2021年11月04日 星期四 18时40分42秒
+> Description:   
+ ************************************************************************/
+
+#include<iostream>
+
+int main()
+{
+
+    std::cout<<"hello,this is the third try"<<std::endl;
+
+    return 0;
+}
